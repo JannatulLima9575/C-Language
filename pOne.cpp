fa574bc84c2fe1bc21e65b48ef48c1b49c6108ca
Jannatul <limajannat1675@gmail.com>
2025-05-03 08:39:27 +0600

    added prolem solving-1

---
+++ pOne.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    count << "Hello World";
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cin >> name;
+    cout << "Hello, " << name;
+    return 0;
+}
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    getline(cin, name);
+    cout << "Hello, " << name;
+    return 0;
+}
