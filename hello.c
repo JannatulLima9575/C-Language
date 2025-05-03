2f42d111c0de85da52ff5b8d7b03c50567e88fce
Jannatul <limajannat1675@gmail.com>
2025-05-03 14:42:22 +0600

    added more problem

--- hello.c
+++ hello.c
@@ -18,3 +18,44 @@ int main() {
 
     return 0;
 }
+
+#include <stdio.h>
+
+int main() {
+    int X, Y;
+
+    scanf("%d %d", &X, &Y);
+
+    printf("X + Y = %d\n", X + Y);
+    printf("X * Y = %d\n", X * Y);
+    printf("X - Y = %d\n", X - Y);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    int A, B, C, D, X;
+
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+
+    X = (A * B) - (C * D);
+    printf("Difference = %d\n", X);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    long long int A, B, C, D, X;
+
+    scanf("%lld %lld %lld %lld", &A, &B, &C, &D);
+
+    X = (A * B) - (C * D);
+
+    printf("Difference = %lld\n", X);
+
+    return 0;
+}
